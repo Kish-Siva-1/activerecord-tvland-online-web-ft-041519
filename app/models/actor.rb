e420050c88549d2159e6130abf35fb41cadbb11a
Kishandth Sivapalasundaram <rodes88@msn.com>
2019-06-05 12:33:15 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -8,7 +8,7 @@ class Actor < ActiveRecord::Base
   
   def list_roles
     binding.pry
-    "#{self.characters.name} - #{show.name}"
+    "#{self.characters.name} - #{self.shows.name}"
   end 
   
 end
